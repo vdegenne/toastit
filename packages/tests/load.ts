a5572bd2595b7ef3ae8f2251491197abbd0406d2
vdegenne <vdegenne@gmail.com>
2024-10-31 18:03:16 +0100

    big restructuration

--- packages/tests/load.ts
+++
@@ -1,9 +0,0 @@
-import {materialDialog} from 'material-3-prompt-dialog';
-import toast from 'toastit';
-
-materialDialog({
-	content: () => 'Hello Material Team',
-});
-
-await new Promise((r) => setTimeout(r, 1000));
-toast('top snackbar!!', 100000);
